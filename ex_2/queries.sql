a27ccc5e70b2b62d5a8a0d5aa3b3504858acc0fe
loginreegerye <reegerye@gmail.com>
2015-07-01 16:57:18 +0300

    Second Exercise

--- ex_2/queries.sql
+++ ex_2/queries.sql
@@ -1,17 +1,17 @@
 select count(u.id), u.first_name
 from users u left outer join users_books ub on u.id = ub.user_id
-where age>20 and age<50 group by u.id having count(u.id)>10
+where age>20 and age<50 group by u.id having count(u.id)>10;
 
 select *
 from users u
-where first_name like '%3%'
+where first_name like '%3%';
 
 select *
-from users unjoin users_books ub on u.id = ub.user_id join books b on ub.book_id=b.id
-where b.title="Book #45"
+from users u join users_books ub on u.id = ub.user_id join books b on ub.book_id=b.id
+where b.title="Book #45";
 
-alter table books add isbestseller bit
+alter table books add isbestseller bit;
 
 update books b
 set isbestseller=1
-where (select count(*) from users_books ub where ub.book_id = b.id)>10
+where (select count(*) from users_books ub where ub.book_id = b.id)>10;
